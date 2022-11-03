c9f730205903d00dae7c6f176a0baaa57e6d681f
douar <mouadouarzong@gmail.com>
2022-11-03 11:08:27 -0500

    init - set up database

---
+++ src/main/java/net/yorksolutions/douarmouacontactsbe/DouarMouaContactsBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.douarmouacontactsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouarMouaContactsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DouarMouaContactsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/douarmouacontactsbe/DouarMouaContactsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.douarmouacontactsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouarMouaContactsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
