a1f24c85e251fe3ec71c4f54c7c76df0354e809f
douar <mouadouarzong@gmail.com>
2022-11-03 11:48:10 -0500

    getter & setters for account id, username, password

--- src/main/java/net/yorksolutions/douarmouacontactsbe/Entities/Account.java
+++ src/main/java/net/yorksolutions/douarmouacontactsbe/Entities/Account.java
@@ -20,4 +20,32 @@ public class Account {
     @JsonProperty
     @JsonIgnore
     private String password;
+
+    public Account() {}
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
