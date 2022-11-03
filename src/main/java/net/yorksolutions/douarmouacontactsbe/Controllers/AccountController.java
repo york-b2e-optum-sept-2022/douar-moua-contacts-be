9e7c3ec979c4d754570060979bbe063c7a7bc680
douar <mouadouarzong@gmail.com>
2022-11-03 13:36:20 -0500

    added createAccount code

--- src/main/java/net/yorksolutions/douarmouacontactsbe/Controllers/AccountController.java
+++ src/main/java/net/yorksolutions/douarmouacontactsbe/Controllers/AccountController.java
@@ -1,19 +1,23 @@
 package net.yorksolutions.douarmouacontactsbe.Controllers;
 
+import net.yorksolutions.douarmouacontactsbe.DTOs.NewAccountRequestDTO;
+import net.yorksolutions.douarmouacontactsbe.Entities.Account;
 import net.yorksolutions.douarmouacontactsbe.Services.AccountService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/account")
 public class AccountController {
 
-    private AccountService accountService;
+    private final AccountService accountService;
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
+    @PostMapping
+    public Account createAccount(@RequestBody NewAccountRequestDTO requestDTO){
+        return this.accountService.createAccount(requestDTO);
+    }
 }
